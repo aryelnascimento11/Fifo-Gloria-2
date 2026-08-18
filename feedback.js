f669a6027ece77c66b275ed7176ccc5ba2e53fd5
Aryel Correa do Nascimento <126723967+aryelnascimento11@users.noreply.github.com>
2026-08-17 21:13:51 -0300

    aplicando banco de dados

--- feedback.js
+++ feedback.js
@@ -1,54 +1,92 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",  async () => {
 
     const list = [];
 
-    const estrelinha = document.getElementById("estrelas");
+    const nota = document.getElementById("estrelas");
     const botaoENV = document.getElementById("btn-feedback");
-    const user = document.getElementById("nomes");
-    const comment = document.getElementById("textos");
+    const nome = document.getElementById("nomes");
+    const mensagem = document.getElementById("textos");
     const feedList = document.getElementById("listasTX");
 
-    botaoENV.addEventListener("click", () => {
-        const usuario = user.value.trim();
-        const comentario = comment.value.trim();
-        const inputss = document.querySelector('input[name="nota"]:checked');
-        const  sol =  inputss ? inputss.value : "";
 
-        if (usuario === "" || comentario === "" || sol === "") return;
+    async function carregarFeed(){
+                const { data, erro } = await db
+            .from("avaliacoes")
+            .select("*")
+            .order("criado_em", { ascending: true })
+            .limit(6)
 
-        list.push({
-            user: usuario,
-            comment: comentario,
-            estrelinha: sol
-        });
+            if(error){
+                console.error("deu errro", error.menssage)
+                return;
+            }
 
-        user.value = "";
-        comment.value = "";
-        
-        
-        if(inputss){
+        mostrarFeed(data)
+    }
+
+    carregarFeed()
+
+    botaoENV.addEventListener("click", async () => {
+        const usuario = nome.value.trim();
+        const comentario = mensagem.value.trim();
+        const inputss = document.querySelector('input[name="result"]:checked');
+        const sol = inputss ?   Number(inputss.value) : null;
+
+        if (usuario === "" || comentario === "" || sol === null) return;
+
+        const { error } = await db
+            .from("avaliacoes")
+            .insert([
+                {
+                    nome: usuario,
+                    mensagem: comentario,
+                    nota: Number(sol)
+                }
+            ])
+
+            if(error){
+                console.error("deiii erro", error.message);
+                return;
+            }
+
+
+        nome.value = "";
+        mensagem.value = "";
+
+
+        if (inputss) {
             inputss.checked = false;
         }
 
-        mostrarFeed();
+
+
+        await carregarFeed();
     });
 
-    function mostrarFeed() {
+    function mostrarFeed(list) {
+            if(!feedList)return;
+        
         feedList.innerHTML = "";
+
+        if(!list) return;
+
         list.forEach((item) => {
             const li = document.createElement("li");
+            const VrEstre = Number(item.nota) || 0;
+            const estrelas = "★" .repeat(VrEstre);
             li.classList.add("feedback-item");
             li.innerHTML = `
-            <span class="banana">${item.estrelinha}</span>
+            <span class="banana">${estrelas}</span>
             <div class="dereto"> 
-                <span>${item.user}:</span>
-                <p>${item.comment}</p >
+                <span>${item.nome}:</span>
+                <p>${item.mensagem}</p >
             
             </div>
             `
-            
+
             feedList.appendChild(li);
         });
     }
 
 });
+
